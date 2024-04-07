56d28aee4900d6bf62e866a6560f5089c08bdca6
yashwant <yash.mangate1@gmail.com>
2024-04-07 13:23:24 +0530

    ADD/UPDATE: updated as per new docs

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,10 +175,10 @@
    * Clients Slider
    */
   new Swiper(".clients-slider", {
-    speed: 400,
+    speed: 1500,
     loop: true,
     autoplay: {
-      delay: 5000,
+      delay: 0,
       disableOnInteraction: false,
     },
     slidesPerView: "auto",
